2e61277caa3fdeb6ed3ebedd4a3877368aec9ed5
Alex Warnes <alex.s.warnes@gmail.com>
2024-03-01 14:25:00 -0600

    testing pt fetch

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -1,10 +1,23 @@
-export function load({ request, getClientAddress }) {
+const fetchTimesByLocation = async (request: any) => {
+  const baseURL = "http://api.aladhan.com/v1/timingsByCity"
+  // http://api.aladhan.com/v1/timingsByCity?city=Dubai&country=United Arab Emirates&method=8
+  const city = request.headers.get("X-Vercel-IP-City");
+  const state = request.headers.get("X-Vercel-IP-Country-Region");
+  const country = request.headers.get("X-Vercel-IP-Country");
+  const method = 2 // ISNA
+  return await fetch(`${baseURL}?/city=${city}&state=${state}&country=${country}&method=${method}`)
+}
+
+export async function load({ request, getClientAddress }) {
   const xvRegion = request.headers.get("X-Vercel-IP-Country-Region");
   const xvCountry = request.headers.get("X-Vercel-IP-Country");
   const xvCity = request.headers.get("X-Vercel-IP-City");
   const xvLat = request.headers.get("X-Vercel-latitude");
   const xvLng = request.headers.get("X-Vercel-longitude");
   const gca = getClientAddress();
+
+  const pt = await fetchTimesByLocation(request)
+  
 
   return {
     locationData: {
@@ -14,6 +27,7 @@ export function load({ request, getClientAddress }) {
       xvLat,
       xvLng,
       gca,
+      pt
     },
   };
 }
